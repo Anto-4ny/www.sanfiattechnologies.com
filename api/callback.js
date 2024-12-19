70370f89f084cab8d3446e0d99e43bc113e4cb3b
Antony Ndambuki <143907986+Anto-4ny@users.noreply.github.com>
2024-12-19 15:45:52 +0300

    Update callback.js

--- api/callback.js
+++ api/callback.js
@@ -1,1 +1,9 @@
-
+export default async function handler(req, res) {
+    if (req.method === 'POST') {
+        const { Body } = req.body;
+        // Your callback logic here
+        res.status(200).send('Callback processed successfully');
+    } else {
+        res.status(405).send('Method Not Allowed');
+    }
+}
