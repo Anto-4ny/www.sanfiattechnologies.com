0c51d1e3ca4ee1612c82f1a21c3ddb89180f8f14
Antony Ndambuki <143907986+Anto-4ny@users.noreply.github.com>
2024-12-24 11:01:42 +0000

    sa

--- script.js
+++ script.js
@@ -164,7 +164,7 @@ document.addEventListener('DOMContentLoaded', () => {
             }
 
             try {
-                const response = await fetch('/api/register', {
+                const response = await fetch('./api/register', {
                     method: 'POST',
                     headers: { 'Content-Type': 'application/json' },
                     body: JSON.stringify({ firstName, lastName, email, password, referralCode }),
